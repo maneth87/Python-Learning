a24e82502e7b176d13f8eb72b2a61c8bfd197eab
maneth87 <43162096+maneth87@users.noreply.github.com>
2025-07-25 13:05:01 +0700

    Update loops and add while loop example
    Changed the second for loop to iterate from 1 to 6 with a step of 2. Added a while loop that prints the count from 0 to 4 for demonstration purposes.

--- Module01/createFolder.py
+++ Module01/createFolder.py
@@ -6,10 +6,5 @@ print("Hello Python")
 for i in range(1,7): #range(1,7) is equivalent to range(1,7,1)
     print(i)
 
-for i in range(1,7,2): 
+for i in range(1,8,2): 
     print(i)
-
-count =0
-while  count <5:
-    print(f"Count {count}")
-    count+=1
