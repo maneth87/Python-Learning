7ae7b70c321d51143859eaf2c7245ba3e76e8e26
maneth87 <somaneth@gmail.com>
2025-07-25 12:36:13 +0700

    Create createFolder.py

---
+++ Module01/createFolder.py
@@ -0,0 +1,10 @@
+print("Hello Python")
+
+#for i in range(6):
+ #   print(i+1)
+
+for i in range(1,7): #range(1,7) is equivalent to range(1,7,1)
+    print(i)
+
+for i in range(1,8,2): 
+    print(i)
